10b565d0fa08f768a8a5b5c218dc906ca7c804b1
Cristopher Coronado Moreira <cristofima@hotmail.com>
2021-04-25 13:00:56 -0500

    Adding validations to Product model

--- StoreDemoApi/Models/Product.cs
+++ StoreDemoApi/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,10 +9,22 @@ namespace StoreDemoApi.Models
     public partial class Product
     {
         public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, 300)]
         public decimal Price { get; set; }
+
+        [Required]
         public int Stock { get; set; }
-        public bool? Enabled { get; set; }
+
+        [Required]
+        public bool Enabled { get; set; }
+
+        [Required]
         public DateTime ExpirationDate { get; set; }
         public string Description { get; set; }
     }
